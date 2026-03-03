e2d5f6c7e7ad2d12f135a34a3ee4a99ac89a0aae
Veer Pratap Singh <xyresiiic@gmail.com>
2026-03-03 10:51:42 +0530

    Add files via upload

---
+++ DataTypes/Premitive.java
@@ -0,0 +1,18 @@
+
+
+public class Premitive {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 3.14f; // f as suffix of float
+        char c = 'A';
+        boolean d = true;
+        long e = 123_456_789L; // L as suffix of long 
+
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + d);
+        System.out.println("Long: " + e);
+    }
+    
+}
---
+++ DataTypes/Refrance.java
@@ -0,0 +1,15 @@
+// also known as reference data types
+
+public class Refrance {
+    public static void main(String[] args) {
+        String name = "Veer";
+        int age = 20;
+        Double height = 5.9;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+      
+    }
+     
+}
